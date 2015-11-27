05d02eda91bed95021b339976b1b379150ba3b4f
Nikola Kolev <koue@chaosophia.net>
2015-11-27 12:06:24 +0200

    Checkout httpd dependency files from OpenBSD cvs.

---
+++ OpenBSD/cvs_checkout_httpd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/usr.sbin/httpd
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/include/blf.h
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libc/crypt/bcrypt.c
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libc/crypt/cryptutil.c
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libtls
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libutil/imsg-buffer.c
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libutil/imsg.c
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libutil/imsg.h
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libssl/src/crypto/o_time.h
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libssl/src/crypto/asn1/a_time_tm.c
+cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libc/gen/vis.c
