2a13acb1f7fbe3d88bb60264b3efbe2508342c3f
Nikola Kolev <koue@chaosophia.net>
2016-10-07 14:29:10 +0200

    Remove libssl files.

--- OpenBSD/cvs_checkout_httpd.sh
+++ OpenBSD/cvs_checkout_httpd.sh
@@ -9,8 +9,6 @@ cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libtls
 cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libutil/imsg-buffer.c
 cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libutil/imsg.c
 cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libutil/imsg.h
-cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libssl/src/crypto/o_time.h
-cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libssl/src/crypto/asn1/a_time_tm.c
 cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/lib/libc/gen/vis.c
 cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/etc/examples/httpd.conf
 cvs -qd anoncvs@ftp5.eu.openbsd.org:/cvs get -P src/usr.bin/htpasswd
