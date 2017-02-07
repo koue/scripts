1197f998aa7472196ae00182ff01cce03a55dac6
Nikola Kolev <koue@chaosophia.net>
2017-02-07 18:25:53 +0100

    Remove libtls. Use libressl-devel as dependency

--- OpenBSD/cvs_checkout_httpd.sh
+++ OpenBSD/cvs_checkout_httpd.sh
@@ -10,13 +10,13 @@ cvs -qd anoncvs@$SERVER:/cvs get -P src/include/blf.h
 cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/crypt/bcrypt.c
 cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/crypt/blowfish.c
 cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/crypt/cryptutil.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libtls
+#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libtls
 cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libutil/imsg-buffer.c
 cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libutil/imsg.c
 cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libutil/imsg.h
 cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/gen/vis.c
 cvs -qd anoncvs@$SERVER:/cvs get -P src/etc/examples/httpd.conf
 cvs -qd anoncvs@$SERVER:/cvs get -P src/usr.bin/htpasswd
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/string/timingsafe_bcmp.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/string/timingsafe_memcmp.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libcrypto/asn1/a_time_tm.c
+#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/string/timingsafe_bcmp.c
+#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/string/timingsafe_memcmp.c
+#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libcrypto/asn1/a_time_tm.c
