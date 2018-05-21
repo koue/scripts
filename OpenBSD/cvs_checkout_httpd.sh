2b1d79066b0d82949d684da4f936eb1c966b924a
Nikola Kolev <koue@chaosophia.net>
2018-05-21 21:22:00 +0200

    Checkout OpenBSD httpd tests

--- OpenBSD/cvs_checkout_httpd.sh
+++ OpenBSD/cvs_checkout_httpd.sh
@@ -5,27 +5,28 @@ else
 	SERVER=ftp.hostserver.de
 fi
 
-cvs -qd anoncvs@$SERVER:/cvs get -P src/usr.sbin/httpd
-cvs -qd anoncvs@$SERVER:/cvs get -P src/include/blf.h
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/crypt/bcrypt.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/crypt/blowfish.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/crypt/cryptutil.c
-#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libtls
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libutil/imsg-buffer.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libutil/imsg.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libutil/imsg.h
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/gen/vis.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/etc/examples/httpd.conf
-cvs -qd anoncvs@$SERVER:/cvs get -P src/usr.bin/htpasswd
-#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/string/timingsafe_bcmp.c
-#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/string/timingsafe_memcmp.c
-#cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libcrypto/asn1/a_time_tm.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/usr.sbin/httpd
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/include/blf.h
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/crypt/bcrypt.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/crypt/blowfish.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/crypt/cryptutil.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libutil/imsg-buffer.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libutil/imsg.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libutil/imsg.h
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/gen/vis.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/etc/examples/httpd.conf
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/usr.bin/htpasswd
+###
+# regress
+###
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/share/mk/bsd.regress.mk
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/regress/usr.sbin/httpd
 ###
 # libevent
 ###
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libevent
-cvs -qd anoncvs@$SERVER:/cvs get -P src/include/asr.h
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/stdlib/reallocarray.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/stdlib/recallocarray.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/asr/asr.c
-cvs -qd anoncvs@$SERVER:/cvs get -P src/lib/libc/asr/asr_private.h
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libevent
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/include/asr.h
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/stdlib/reallocarray.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/stdlib/recallocarray.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/asr/asr.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/asr/asr_private.h
