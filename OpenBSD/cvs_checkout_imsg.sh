5aedc87d21910cf2105cb40272f8332b49d35d30
Nikola Kolev <koue@chaosophia.net>
2018-06-21 14:56:08 +0000

    Add imsg checkout scripts.

---
+++ OpenBSD/cvs_checkout_imsg.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ ! -z ${1} ]; then
+	SERVER=${1}
+else
+	SERVER=ftp.hostserver.de
+fi
+
+# libutil
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libutil/imsg-buffer.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libutil/imsg.c
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libutil/imsg.h
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libutil/imsg_init.3
+# dependencies
+cvs -qd anoncvs@${SERVER}:/cvs get -P src/lib/libc/stdlib/recallocarray.c
+
+#find src/ -type d -name CVS -exec rm -rf {} \;
