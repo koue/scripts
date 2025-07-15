1729d285ccc4788c228cdc9204d9b515e6cf6c81
Nikola Kolev <koue@chaosophia.net>
2025-07-15 09:57:20 +0000

    FreeBSD local

---
+++ FreeBSD/local/git.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+V=2.50.1
+
+fetch https://www.kernel.org/pub/software/scm/git/git-${V}.tar.gz
+tar zxf git-${V}.tar.gz && cd git-${V}/
+./configure --without-openssl --without-expat --without-curl --without-tcltk --prefix=/opt/git
+gmake && gmake install
