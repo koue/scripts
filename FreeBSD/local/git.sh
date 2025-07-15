f4a7ad0365464e8971545504a7732becfb353394
Nikola Kolev <koue@chaosophia.net>
2025-07-15 10:11:38 +0000

    git.sh without-iconv

--- FreeBSD/local/git.sh
+++ FreeBSD/local/git.sh
@@ -4,5 +4,11 @@ V=2.50.1
 
 fetch https://www.kernel.org/pub/software/scm/git/git-${V}.tar.gz
 tar zxf git-${V}.tar.gz && cd git-${V}/
-./configure --without-openssl --without-expat --without-curl --without-tcltk --prefix=/opt/git
+./configure \
+	--without-openssl \
+	--without-expat \
+	--without-curl  \
+	--without-tcltk \
+	--without-iconv \
+	--prefix=/opt/git
 gmake && gmake install
